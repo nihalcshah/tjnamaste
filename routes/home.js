06f67fb248d1b82eef1f5054ffd9ed668b90d15d
nihalcshah <nihalcshah@gmail.com>
2022-12-20 19:31:18 -0500

    c2

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,27 @@
 const express = require('express');
 var router = express.Router();
 
+var events = [
+    {
+        "event_name": "Movie Night",
+        "date": "Upcoming F"
+    },
+];
+
 router.get('/', function(req, res){
     res.render('home');
 });
+
+router.get('/about',function(req, res){
+    res.render('about')
+});
+
+let officers = {
+    // "officers":{
+    //     ""
+    // }
+    
+}
+
 
 module.exports = router;
