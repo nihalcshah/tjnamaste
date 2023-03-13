640fff6949ebd1714818ed5a6b7c469e669c4a6d
nihalcshah <nihalcshah@gmail.com>
2023-03-12 21:08:12 -0400

    Events adding

--- routes/home.js
+++ routes/home.js
@@ -80,88 +80,88 @@ router.get('/oauth', async function(req, res) {
     res.redirect("http://localhost:8080/")
 });
 
-function downloader_promise(url) {
-    return new Promise( function(resolve,reject){
-      https.get(url, function(response) {
-        var rawData = '';
-        response.on('data', function(chunk) { rawData+=chunk; })
-        response.on('end', function() { resolve(rawData); })
-      }).on('error', function(e) {
-        reject(e)
-      });
-    })
-}
+// function downloader_promise(url) {
+//     return new Promise( function(resolve,reject){
+//       https.get(url, function(response) {
+//         var rawData = '';
+//         response.on('data', function(chunk) { rawData+=chunk; })
+//         response.on('end', function() { resolve(rawData); })
+//       }).on('error', function(e) {
+//         reject(e)
+//       });
+//     })
+// }
 
-router.get('/edit', async function(req, res) { 
-    validUsers = [
-        '2023nshah',
-        '2023pshan',
-        '2023nshyamala',
-        '2023aaneja',
-        '2023njayapal',
-        '2025emoorthy',
-        '2024lmedhuri',
-    ]
+// router.get('/edit', async function(req, res) { 
+//     validUsers = [
+//         '2023nshah',
+//         '2023pshan',
+//         '2023nshyamala',
+//         '2023aaneja',
+//         '2023njayapal',
+//         '2025emoorthy',
+//         '2024lmedhuri',
+//     ]
 
-    let token = req.session.accessToken.access_token
-    const profile_url = `https://ion.tjhsst.edu/api/profile?format=json&access_token=${token}`
-    let profile_string = await downloader_promise(profile_url);
-    let profile = JSON.parse(profile_string)
-    if('detail' in profile&& profile['detail']=='Authentication credentials were not provided.'){
-      req.session.authenticated = false;
-      req.session.authorized = false;
-      res.clearCookie("accessToken");
-      res.redirect('/');
-      return
-    } 
-    console.log(profile['ion_username'])
-    console.log( validUsers.includes(profile['ion_username']))
+//     let token = req.session.accessToken.access_token
+//     const profile_url = `https://ion.tjhsst.edu/api/profile?format=json&access_token=${token}`
+//     let profile_string = await downloader_promise(profile_url);
+//     let profile = JSON.parse(profile_string)
+//     if('detail' in profile&& profile['detail']=='Authentication credentials were not provided.'){
+//       req.session.authenticated = false;
+//       req.session.authorized = false;
+//       res.clearCookie("accessToken");
+//       res.redirect('/');
+//       return
+//     } 
+//     console.log(profile['ion_username'])
+//     console.log( validUsers.includes(profile['ion_username']))
 
-    if(validUsers.includes(profile['ion_username'])){
-        req.session.authorized = true;
-        var sqlQuery = 'select * from events;'
-        let k = await sqlPromise(pool, sqlQuery)
+//     if(validUsers.includes(profile['ion_username'])){
+//         req.session.authorized = true;
+//         var sqlQuery = 'select * from events;'
+//         let k = await sqlPromise(pool, sqlQuery)
 
-        var totallist = [
+//         var totallist = [
 
-        ]
+//         ]
 
-        k.forEach(function (item, index) {
-            const evdate = new Date(item['eventtime']);
-            let temp = evdate.toDateString()+" | "+item['eventtype']+" | "+item['Event']
-            totallist.push([temp, item["Event"]])
+//         k.forEach(function (item, index) {
+//             const evdate = new Date(item['eventtime']);
+//             let temp = evdate.toDateString()+" | "+item['eventtype']+" | "+item['Event']
+//             totallist.push([temp, item["Event"]])
 
-        });
-        console.log(totallist);
-        res.render('edit', {'name': profile['first_name'], "logged_in":true, "list":totallist})
-        return
-    }else{
-        req.session.authenticated = false;
-        req.session.authorized = false;
-        res.clearCookie("accessToken");
-        res.redirect('/')
-        return
-    }
+//         });
+//         console.log(totallist);
+//         res.render('edit', {'name': profile['first_name'], "logged_in":true, "list":totallist})
+//         return
+//     }else{
+//         req.session.authenticated = false;
+//         req.session.authorized = false;
+//         res.clearCookie("accessToken");
+//         res.redirect('/')
+//         return
+//     }
     
-});
+// });
 
-router.post('/addevent', async function(req, res) { 
-    var sqlQuery = 'insert into events (eventtype, event, eventtime) values (?, ?, ?);'
-    console.log(sqlQuery)
-    console.log(req.body['eventtype'])
-    console.log(req.body['eventname'])
-    console.log(req.body['date'])
-    let k = await sqlPromise(pool, sqlQuery, [req.body['eventtype'], req.body['eventname'], req.body['date']])
-    console.log(k);
-    console.log("hi")
-    res.redirect('/edit')
-  });
+// router.post('/addevent', async function(req, res) { 
+//     var sqlQuery = 'insert into events (eventtype, event, eventtime) values (?, ?, ?);'
+//     console.log(sqlQuery)
+//     console.log(req.body['eventtype'])
+//     console.log(req.body['eventname'])
+//     console.log(req.body['date'])
+//     let k = await sqlPromise(pool, sqlQuery, [req.body['eventtype'], req.body['eventname'], req.body['date']])
+//     console.log(k);
+//     console.log("hi")
+//     res.redirect('/edit')
+//   });
 
-  router.post('/remove', async function(req, res) { 
-    var sqlQuery = 'DELETE FROM events WHERE event=?;'
-    let k = await sqlPromise(pool, sqlQuery, [req.body['event']])
-    res.send("3")
-  });
+//   router.post('/remove', async function(req, res) { 
+//     var sqlQuery = 'DELETE FROM events WHERE event=?;'
+//     let k = await sqlPromise(pool, sqlQuery, [req.body['event']])
+//     res.send("3")
+//   });
 
 
 module.exports = router;
